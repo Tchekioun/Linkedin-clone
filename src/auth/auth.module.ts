22a21e04995c6c06a935b9a712ab25770616466d
Abderraouf <abdelraouffinch@gmail.com>
2023-02-07 22:48:27 +0100

    Fix modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './auth.controller';
 import { PasswordService } from './services/password.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  providers: [AuthService, PasswordService],
+  providers: [AuthService, UsersService, PasswordService, LocalStrategy],
   controllers: [AuthController],
   exports: [PasswordService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,5 +6,6 @@ import { PasswordService } from 'src/auth/services/password.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService, PasswordService],
+  exports: [UsersService],
 })
 export class UsersModule {}
