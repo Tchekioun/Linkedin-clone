4c80821cd3d887c1a7563f8ae007ea8e18edaa6b
Abderraouf <abdelraouffinch@gmail.com>
2023-02-07 23:16:54 +0100

    Add JwtModule to login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,24 @@ import { AuthController } from './auth.controller';
 import { PasswordService } from './services/password.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { UsersService } from 'src/users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { env } from 'process';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  providers: [AuthService, UsersService, PasswordService, LocalStrategy],
+  imports: [
+    JwtModule.register({
+      secret: env.JWT_SECRET,
+      signOptions: { expiresIn: '300s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    UsersService,
+    PasswordService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
   exports: [PasswordService],
 })
